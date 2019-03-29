00028b415158bc596b25313e73754600262baeb8
cowSec16 <41345148+JordanMiller16@users.noreply.github.com>
2019-03-29 10:42:41 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 sudo dpkg -i osquery.deb;
 sudo dpkg -i /var/osquery/osqueryBinary.deb;
 
-sudo osqueryd --config_plugin=tls --tls_hostname=fleet.ir.cfasec.com --enroll_tls_endpoint=/api/v1/osquery/enroll --config_tls_endpoint=/api/v1/osquery/config --config_refresh=10 --enroll_secret_path=/var/local/fleet --daemonize --force
+sudo osqueryd --config_plugin=tls --tls_hostname=fleet.ir.cfasec.com --enroll_tls_endpoint=/api/v1/osquery/enroll --config_tls_endpoint=/api/v1/osquery/config --config_refresh=10 --enroll_secret_path=/var/local/fleet --logger_min_status=3 --logger_min_stderr=2 --daemonize --force
